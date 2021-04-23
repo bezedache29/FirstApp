630d244b313816df278ff54022ee24c43e83f6e2
Christophe Salou <bezedache29@gmail.com>
2021-04-23 15:40:41 +0200

    Envoie / reception des données formulaire

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -1,17 +1,36 @@
 package com.example.firstapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import androidx.appcompat.app.AppCompatActivity
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
-}
 
-class MyButton: OnClickListener {
-    override fun onClick() {
-        setContentView(R.layout.activity_main2)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            this.onClick(v = null)
+        }
     }
+
+    override fun onClick(v: View?) {
+//        input lastname
+        val lastname: EditText = findViewById(R.id.editTextTextPersonName)
+        val lastnameValue: String = lastname.getText().toString()
+//        input firstname
+        val firstname: EditText = findViewById(R.id.editTextTextPersonName2)
+        val firstnameValue: String = firstname.getText().toString()
+
+        val i = Intent(this, MainActivity2::class.java)
+        i.putExtra("lastname", lastnameValue)
+        i.putExtra("firstname", firstnameValue)
+        startActivity(i)
+    }
+
 }
--- app/src/main/java/com/example/firstapp/MainActivity2.kt
+++ app/src/main/java/com/example/firstapp/MainActivity2.kt
@@ -1,6 +1,7 @@
 package com.example.firstapp
 
 import android.os.Bundle
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -10,11 +11,20 @@ class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
-        setSupportActionBar(findViewById(R.id.toolbar))
+//        setSupportActionBar(findViewById(R.id.toolbar))
 
-        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
-            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
-        }
+//        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+//            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                    .setAction("Action", null).show()
+//        }
+
+        val lastname: String? = intent.getStringExtra("lastname")
+        val firstname: String? = intent.getStringExtra("firstname")
+
+        val lastnameTextview: TextView = findViewById(R.id.lastnameValue)
+        val firstnameTextview: TextView = findViewById(R.id.firstnameValue)
+
+        lastnameTextview.setText(lastname)
+        firstnameTextview.setText(firstname)
     }
 }
