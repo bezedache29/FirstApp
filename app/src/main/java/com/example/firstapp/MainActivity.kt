737db014a1d6f2bfd190474344dbd6d941b50b01
Christophe Salou <bezedache29@gmail.com>
2021-04-23 16:17:58 +0200

    Messages d'erreur

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -5,7 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.text.trimmedLength
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -17,6 +19,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
     lateinit var lastname: EditText
     lateinit var firstname: EditText
     lateinit var button: Button
+    lateinit var errorMessage: TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -25,6 +28,7 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         button = findViewById(R.id.button)
         lastname = findViewById(R.id.editTextTextPersonName)
         firstname = findViewById(R.id.editTextTextPersonName2)
+        errorMessage = findViewById(R.id.errorMessage)
 
         button.setOnClickListener {
             this.onClick(v = null)
@@ -33,9 +37,12 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     override fun onClick(v: View?) {
         val i = Intent(this, MainActivity2::class.java)
-        i.putExtra(LASTNAMEKEY, lastname.getText().toString())
-        i.putExtra(FIRSTNAMEKEY, firstname.getText().toString())
-        startActivity(i)
+        if ((lastname.getText().toString().trimmedLength() <= 0) || (firstname.getText().toString().trimmedLength() <= 0)) {
+            errorMessage.text = "Tous les champs doivent être rempli"
+        } else {
+            i.putExtra(LASTNAMEKEY, lastname.getText().toString())
+            i.putExtra(FIRSTNAMEKEY, firstname.getText().toString())
+            startActivity(i)
+        }
     }
-
 }
