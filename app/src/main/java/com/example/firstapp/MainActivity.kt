4326bc22a4502fdf6e437428e877790f3c158e56
Christophe Salou <bezedache29@gmail.com>
2021-04-30 14:38:36 +0200

    View-binding

--- app/src/main/java/com/example/firstapp/MainActivity.kt
+++ app/src/main/java/com/example/firstapp/MainActivity.kt
@@ -3,45 +3,38 @@ package com.example.firstapp
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.trimmedLength
+import com.example.firstapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
-    companion object{
-        val LASTNAMEKEY = "lastname"
-        val FIRSTNAMEKEY = "firstname"
+    companion object {
+        const val LASTNAMEKEY = "lastname"
+        const val FIRSTNAMEKEY = "firstname"
     }
 
-    lateinit var lastname: EditText
-    lateinit var firstname: EditText
-    lateinit var button: Button
-    lateinit var errorMessage: TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        button = findViewById(R.id.button)
-        lastname = findViewById(R.id.editTextTextPersonName)
-        firstname = findViewById(R.id.editTextTextPersonName2)
-        errorMessage = findViewById(R.id.errorMessage)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             this.onClick(v = null)
         }
     }
 
     override fun onClick(v: View?) {
         val i = Intent(this, MainActivity2::class.java)
-        if ((lastname.getText().toString().trimmedLength() <= 0) || (firstname.getText().toString().trimmedLength() <= 0)) {
-            errorMessage.text = "Tous les champs doivent être rempli"
+        if ((binding.editTextTextPersonName.text.toString().trimmedLength() <= 0) || (binding.editTextTextPersonName2.text.toString().trimmedLength() <= 0)) {
+            binding.errorMessage.text = "Tous les champs doivent être rempli"
         } else {
-            i.putExtra(LASTNAMEKEY, lastname.getText().toString())
-            i.putExtra(FIRSTNAMEKEY, firstname.getText().toString())
+            i.putExtra(LASTNAMEKEY, binding.editTextTextPersonName.text.toString())
+            i.putExtra(FIRSTNAMEKEY, binding.editTextTextPersonName2.text.toString())
             startActivity(i)
         }
     }
--- app/src/main/java/com/example/firstapp/MainActivity2.kt
+++ app/src/main/java/com/example/firstapp/MainActivity2.kt
@@ -1,24 +1,26 @@
 package com.example.firstapp
 
 import android.os.Bundle
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.firstapp.MainActivity.Companion.FIRSTNAMEKEY
 import com.example.firstapp.MainActivity.Companion.LASTNAMEKEY
+import com.example.firstapp.databinding.ActivityMain2Binding
 
 class MainActivity2 : AppCompatActivity() {
 
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
 
         val lastname: String? = intent.getStringExtra(LASTNAMEKEY)
         val firstname: String? = intent.getStringExtra(FIRSTNAMEKEY)
 
-        val lastnameTextview: TextView = findViewById(R.id.lastnameValue)
-        val firstnameTextview: TextView = findViewById(R.id.firstnameValue)
-
-        lastnameTextview.text = lastname
-        firstnameTextview.text = firstname
+        binding.lastnameValue.text = lastname
+        binding.firstnameValue.text = firstname
     }
 }
